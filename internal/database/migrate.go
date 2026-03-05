4e0dfca196acacf55733caad40cdb686830b3f5a
Ramish Khan <ramishk12@gmail.com>
2026-03-05 00:18:44 -0800

    Update update_at in UTC

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -46,7 +46,7 @@ func Migrate() error {
 		`CREATE OR REPLACE FUNCTION update_updated_at_column()
 		RETURNS TRIGGER AS $$
 		BEGIN
-			NEW.updated_at = CURRENT_TIMESTAMP;
+			NEW.updated_at = (CURRENT_TIMESTAMP AT TIME ZONE 'UTC');
 			RETURN NEW;
 		END;
 		$$ language 'plpgsql'`,
